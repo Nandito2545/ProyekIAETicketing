6aa577500908777ac515b6f54a84ec5765ef8c15
Nandito2545 <bangdito87@gmail.com>
2025-11-08 17:14:04 +0700

    grpc config

--- api-gatewey/src/config/grpcClients.js
+++ api-gatewey/src/config/grpcClients.js
@@ -5,7 +5,13 @@ const path = require('path');
 const loadProto = (filename) => {
   const packageDefinition = protoLoader.loadSync(
     path.join(__dirname, '../proto', filename),
-    { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true }
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true,
+    }
   );
   return grpc.loadPackageDefinition(packageDefinition);
 };
@@ -15,27 +21,28 @@ const eventProto = loadProto('event.proto');
 const paymentProto = loadProto('payment.proto');
 const notificationProto = loadProto('notification.proto');
 
-const userClient = new userProto.UserService(
+// **PERBAIKAN: akses service melalui package name**
+const userClient = new userProto.user.UserService(
   `${process.env.USER_SERVICE_HOST}:${process.env.USER_SERVICE_PORT}`,
   grpc.credentials.createInsecure()
 );
 
-const eventClient = new eventProto.EventService(
+const eventClient = new eventProto.event.EventService(
   `${process.env.EVENT_SERVICE_HOST}:${process.env.EVENT_SERVICE_PORT}`,
   grpc.credentials.createInsecure()
 );
 
-const ticketClient = new eventProto.TicketService(
+const ticketClient = new eventProto.event.TicketService(
   `${process.env.TICKET_SERVICE_HOST}:${process.env.TICKET_SERVICE_PORT}`,
   grpc.credentials.createInsecure()
 );
 
-const paymentClient = new paymentProto.PaymentService(
+const paymentClient = new paymentProto.payment.PaymentService(
   `${process.env.PAYMENT_SERVICE_HOST}:${process.env.PAYMENT_SERVICE_PORT}`,
   grpc.credentials.createInsecure()
 );
 
-const notificationClient = new notificationProto.NotificationService(
+const notificationClient = new notificationProto.notification.NotificationService(
   `${process.env.NOTIFICATION_SERVICE_HOST}:${process.env.NOTIFICATION_SERVICE_PORT}`,
   grpc.credentials.createInsecure()
 );
@@ -45,5 +52,5 @@ module.exports = {
   eventClient,
   ticketClient,
   paymentClient,
-  notificationClient
+  notificationClient,
 };
